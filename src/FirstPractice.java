549530d6c3f55dacc440c4afe4e30179a23a701c
TheFreeMan21 <99295030+TheFreeMan21@users.noreply.github.com>
2023-03-17 14:20:05 +0100

    New Changes

---
+++ src/FirstPractice.java
@@ -0,0 +1,39 @@
+public class FirstPractice {
+
+    public static void main(String[] args){
+
+        byte haste = 55;
+        byte criticalStrikeChance = 0;
+
+        short armor = 106;
+        short magicResist = 67;
+        short damage = 112;
+        short abilityPower = 652;
+        short speed = 390;
+        short blueEssencePrice = 450;
+        short life = 3291;
+
+        int coins = 64112;
+
+        float attackSpeed = 0.86F;
+
+        boolean isJungle = true;
+
+        System.out.println("Champion Name: Nunu & Willump");
+        System.out.println("Blue Essence price: "+blueEssencePrice);
+        System.out.println("Late game stats while being full built");
+        System.out.println("Life: "+life);
+        System.out.println("Armor: "+ armor);
+        System.out.println("Magic Resist: " + magicResist);
+        System.out.println("Damage: "+damage);
+        System.out.println("Ability Power: "+abilityPower);
+        System.out.println("Attack Speed:" + attackSpeed);
+        System.out.println("Speed: " + speed);
+        System.out.println("Haste: "+ haste);
+        System.out.println("Critical Strike Chance: " + criticalStrikeChance);
+        System.out.println("Current Coins: "+coins);
+        System.out.println("This champion is played in Jungle: " + isJungle);
+
+    }
+
+}
