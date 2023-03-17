910f9da59e6302c449a6c66232c988f56e902351
TheFreeMan21 <99295030+TheFreeMan21@users.noreply.github.com>
2023-03-17 21:46:29 +0100

    New Changes

--- src/OperatorsApp.java
+++ src/OperatorsApp.java
@@ -7,22 +7,43 @@ public class OperatorsApp {
         int moneyInTheBank = 100000;
         boolean isDamaged = true;
 
-        System.out.println("Price of a " + carModel + ": $" + price);
+        System.out.println("Price of a " + carModel + ": $ "  + price);
 
         int increasedPrice = price + 1000;
-        System.out.println("The increased price of a " + carModel + ": $" + increasedPrice);
+        System.out.println("The increased price of a " + carModel + ": $"  + increasedPrice);
 
         int decreasedPrice = price - 1000;
-        System.out.println("The decreased price of a " + carModel + ": $" + decreasedPrice);
+        System.out.println("The decreased price of a " + carModel + ": $ " + decreasedPrice);
 
         int twoCarsPrice = price*2;
-        System.out.println("Two " + carModel + ": $" + twoCarsPrice);
+        System.out.println("Two " + carModel + ": $ " + twoCarsPrice);
 
         int dodgesYouCanBuy = moneyInTheBank/price;
         System.out.println("From the money we have in the bank we can buy " + dodgesYouCanBuy + " " + carModel);
 
         int moneyRemaining = moneyInTheBank%price;
         System.out.println("money we would remain after buying " + dodgesYouCanBuy + " "+ carModel + ": $" + moneyRemaining);
+
+        System.out.println();
+
+
+        int priceNegative = -14999;
+        int priceNegativeWithPlusSign = + priceNegative;
+        System.out.println("Negative with plus sign: $"  + priceNegativeWithPlusSign);
+
+        int priceNegativeWithMinusSign = - priceNegative;
+        System.out.println("Negative with minus sign: $ " + priceNegativeWithMinusSign);
+
+        int priceOneDollarIncrease = ++price;
+        System.out.println("Price after one dollar increase: $ "+priceOneDollarIncrease);
+
+        int priceOneDollarDecrease = --price;
+        System.out.println("Price after one dollar decrease: $ "+priceOneDollarDecrease);
+
+
+        System.out.println("This car is damaged: " + !isDamaged);
+
+        System.out.println();
 
     }
 
